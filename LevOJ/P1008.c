7d20fc72704b04d5d3ae72db91ec43542ca42aaa
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-10-09 19:29:09 +0800

    Update P1008.c

--- LevOJ/P1008.c
+++ LevOJ/P1008.c
@@ -3,8 +3,8 @@ int main(){
 	int n,i;
 	while(scanf("%d",&n)==1){
 		for(i=2;i<=n/2;i++)
-			if(n%i==0){printf("No");goto no;}
-		printf("Yes");
+			if(n%i==0){printf("No\n");goto no;}
+		printf("Yes\n");
 		no:;
 	}
 	return 0;
