6e8a0afbfd1c0c4d35715119f76ebade114003d3
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-10-09 19:32:55 +0800

    Update P1043.c

--- LevOJ/P1043.c
+++ LevOJ/P1043.c
@@ -4,8 +4,8 @@ int main(){
 	while(scanf("%d%d%d",&a,&b,&c)==3){
 		if(a<b){a^=b;b^=a;a^=b;}
 		if(a<c){a^=c;c^=a;a^=c;}//max:a
-		if(a*a==b*b+c*c&&a<b+c)printf("Yes\n");
-		else printf("N0\n");
+		if(a*a==b*b+c*c&&a<b+c)printf("yes\n");
+		else printf("no\n");
 	}
 	return 0;
 }
