1f6dbccccc9dee72520f2540fd83c847f6e3becc
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-10-26 21:11:20 +0800

    Add files via upload

---
+++ POJ/NOI/1.11.1.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+int main(){
+	int a[100001],n,m,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	for(scanf("%d",&m);m;m--){
+		int l=0,r=n-1,t=1,mid,q;
+		scanf("%d",&q);
+		while(l<r-1){
+			mid=(l+r)/2;
+			if(a[mid]>q)r=mid;
+			if(a[mid]<q)l=mid;
+			if(a[mid]==q){
+				printf("%d\n",q);
+				t=0;
+				break;
+			}
+		}
+		if(t){
+			if(q-a[l]>a[r]-q)
+				printf("%d\n",a[r]);
+			else printf("%d\n",a[l]);
+		}
+	}
+	return 0;
+}
---
+++ POJ/NOI/1.11.2.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+double f(double x){
+	return ((((x-15)*x+85)*x-225)*x+274)*x-121;
+}
+int main(){
+	double l=1.5,r=2.4,m;
+	while(r-l>0.0000001){
+		m=(l+r)/2;
+		if(f(m)>0)l=m;
+		if(f(m)<0)r=m;
+	}
+	printf("%.6f",m);
+	return 0;
+}
