78abf27d3762a49229f228955015164818ca4bb5
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-10-31 00:14:08 +0800

    Add files via upload

---
+++ POJ/NOI/1.11.4.c
@@ -0,0 +1,39 @@
+﻿#include<stdio.h>
+int s[10001],n,k;
+int fine(int x){
+	int p[10001],i=0,r=k;
+	for(;i<n;i++)p[i]=s[i];
+	i=0;
+	while(r){
+		p[i]-=x;
+		if(p[i]<0)	i++;
+		else		r--;
+		if(i>=n)return 0;
+	}
+	return 1;
+}
+int main(){
+	int i=0;
+	float in;
+	scanf("%d%d",&n,&k);
+	for(;i<n;i++){
+		scanf("%f",&in);
+		s[i]=100*in;
+	}
+	if(!fine(1)){
+		printf("0.00");
+		return 0;
+	}
+	int l=1,r=10000001,m;
+	while(r-l>1){
+		m=(l+r)/2;
+		if(fine(m))
+			l=m;
+		else r=m;
+	}
+	if(!fine(m))m--;
+	float ans=m;
+	ans/=100;
+	printf("%.2f",ans);
+	return 0;
+}
---
+++ POJ/NOI/1.11.7.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+int n,a[100001];
+int comp(const void*a,const void*b){
+	return *(int*)a-*(int*)b;
+}
+int main(){
+	int t=1,i=0,j,m;
+	scanf("%d",&n);
+	for(;i<n;i++)scanf("%d",&a[i]);
+	scanf("%d",&m);
+	qsort(a,n,sizeof(int),comp);
+	for(i=0;i<n;i++){
+		int b=m-a[i],l=i+1,r=n-1,m;
+		while(l<=r){
+			m=(l+r)/2;
+			if(a[m]==b){
+				printf("%d %d",a[i],b);
+				i=n;
+				t=0;
+				break;
+			}
+			if(a[m]>b)r=m-1;
+			if(a[m]<b)l=m+1;
+		}
+	}
+	if(t)printf("No");
+	return 0;
+}
