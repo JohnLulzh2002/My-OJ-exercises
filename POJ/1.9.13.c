fc202d10c5d145ea675c371198f2b75b7a00cc54
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-10-31 11:45:40 +0800

    Add files via upload

---
+++ POJ/1.9.13.c
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main(){
+	int a[20001],c[101]={},i=0,n,max=0;
+	for(scanf("%d",&n);i<n;i++)scanf("%d",&a[i]);
+	for(i=0;i<n;i++){
+		if(!c[a[i]])printf("%d ",a[i]);
+		c[a[i]]++;
+	}
+	return 0;
+}
