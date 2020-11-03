2c151bb3b4da7c561c34d64344c5860687627a06
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-11-03 18:09:38 +0800

    Add files via upload

---
+++ LevOJ/P1536.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+int main(){
+	int T,i,a,b;
+	//while(scanf("%d",&T)==1){
+	scanf("%d",&T);
+	while(T--){
+		scanf("%d%d",&a,&b);
+		for(i=1;1;i++){
+			if(i%2){
+				a-=i;
+				if(a<0){
+					printf("A\n");
+					break;
+				}
+			}
+			else{
+				b-=i;
+				if(b<0){
+					printf("B\n");
+					break;
+				}
+			}
+	}	}
+	//}
+	return 0;
+}
