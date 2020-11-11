c846c93050b6399a934966d25fffe41e385ee634
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-11-11 11:26:33 +0800

    Add files via upload

---
+++ POJ/NOI/2.1.1812.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int tri(int a){return a*a*a;}
+int main(){
+	int n,a,b,c,d;
+	scanf("%d",&n);
+	for(a=3;a<=n;a++)
+	for(b=2;b<a;b++)
+	for(c=b;c<a;c++)
+	for(d=c;d<a;d++)
+	if(tri(a)==tri(b)+tri(c)+tri(d))
+		printf("Cube = %d, Triple = (%d,%d,%d)\n",a,b,c,d);
+	return 0;
+}
---
+++ POJ/NOI/2.1.1813.c
@@ -0,0 +1,61 @@
+﻿#include<stdio.h>
+short a[6][7];
+int p[7]={};
+void press(int x,int y){
+	a[x][y]=1-a[x][y];
+	if(x)a[x-1][y]=1-a[x-1][y];
+	if(x-4)a[x+1][y]=1-a[x+1][y];
+	if(y)a[x][y-1]=1-a[x][y-1];
+	if(y-5)a[x][y+1]=1-a[x][y+1];
+}
+void next(){
+	int i=0;
+	for(p[0]++;p[i]>1;i++){
+		p[i]=0;
+		p[i+1]++;
+	}
+}
+int main(){
+	int i=0,j,n,o[6][7];
+	for(i=0;i<5;i++)for(j=0;j<6;j++)
+		scanf("%d",&o[i][j]);
+	while(1){
+		for(i=0;i<5;i++)for(j=0;j<6;j++)
+			a[i][j]=o[i][j];
+		for(i=0;i<6;i++)
+			if(p[i])
+				press(0,i);
+		for(i=1;i<5;i++)for(j=0;j<6;j++)
+			if(a[i-1][j])
+				press(i,j);
+		
+		int t=1;
+		for(i=0;i<6;i++)
+			if(a[4][i])t=0;
+		
+		if(t){
+			for(i=0;i<5;i++)for(j=0;j<6;j++)
+				a[i][j]=o[i][j];
+			for(i=0;i<6;i++){
+				if(p[i]){
+					press(0,i);
+					printf("1 ");
+				}else
+					printf("0 ");
+			}
+			printf("\n");
+			for(i=1;i<5;i++){
+				for(j=0;j<6;j++)
+					if(a[i-1][j]){
+						press(i,j);
+						printf("1 ");
+					}else
+						printf("0 ");
+				printf("\n");
+			}
+			break;
+		}
+		next();
+	}
+	return 0;
+}
