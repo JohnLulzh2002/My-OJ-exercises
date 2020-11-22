2d8bc9f84adf5a0672e14793692da015df057cee
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2020-11-22 20:25:10 +0800

    Create P1601.c

---
+++ Luogu/P1601.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char a[201],b[201];
+	int la,lb,l,i=0,p=0,c[201];
+	scanf("%s %s",&a,&b);
+	la=strlen(a);	lb=strlen(b);
+	for(l=la>lb?la:lb;i<=l;i++){
+		if(i<la)p+=(int)a[la-i-1]-'0';
+		if(i<lb)p+=(int)b[lb-i-1]-'0';
+		if(p<10){
+			c[i]=p;
+			p=0;			
+		}
+		else{
+			c[i]=p-10;
+			p=1;
+		}
+	}
+	for(i=l;c[i]==0;i--);
+	if(i<0)printf("0");
+	for(;i>=0;i--)printf("%d",c[i]);
+	return 0;
+}
