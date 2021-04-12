ba74e17ffe041c1f8671862bd422725c6961aa03
JohnLu-HP-Laptop <lzh20020204@126.com>
2021-04-12 23:54:14 +0800

    Update P5730-gen.py

--- Luogu/P5730-gen.py
+++ Luogu/P5730-gen.py
@@ -2,8 +2,9 @@ a=[]
 for i in range(5):
     a.append(input())
 for i in range(5):
-    print('\n\t{',end='')
+    print('\t{',end='')
     for j in range(10):
-        print('{\'%c\',\'%c\',\'%c\'}'%(a[i][j*4],a[i][j*4+1],a[i][j*4+2]),end='')
+        print("{'%c','%c','%c'}"%(a[i][j*4],a[i][j*4+1],a[i][j*4+2]),end='')
         if j<9:
             print(',',end='')
+    print('},')
