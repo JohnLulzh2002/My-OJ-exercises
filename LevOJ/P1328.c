365a0ffe68750e3f58e142c57b07ad5c590a17c0
JohnLu-HP-Laptop <lzh20020204@126.com>
2021-11-20 17:53:02 +0800

    Update P1328.c

--- LevOJ/P1328.c
+++ LevOJ/P1328.c
@@ -14,16 +14,12 @@ int main(){
 			s+=a;
 		}
 		m1=m2=m3=0;
-		scanf("%d",&a);
-		while(a+1){
+		while(scanf("%d",&a),a+1){
 			if(a>m1)swap(&a,&m1);
 			if(a>m2)swap(&a,&m2);
 			if(a>m3)swap(&a,&m3);
-			scanf("%d",&a);
 		}
-		s+=m1+m2+m3;
-		printf("%d\n",s);
+		printf("%d\n",s+m1+m2+m3);
 	}
-	
 	return 0;
 }
