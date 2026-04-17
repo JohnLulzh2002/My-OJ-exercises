157a88060f0fcd9132ca8706688c3815a7a55cf7
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2026-04-17 09:50:02 +0800

    Create 3167.cs
    力扣每日（4.17）

---
+++ Leetcode/3167.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+	static int Reverse(int n)
+	{
+		int ans = 0;
+		while (n > 0)
+		{
+			ans = ans * 10 + n % 10;
+			n /= 10;
+		}
+		return ans;
+	}
+	public int MinMirrorPairDistance(int[] nums)
+	{
+		int ans = int.MaxValue;
+		Dictionary<int, int> d = [];
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (d.TryGetValue(nums[i], out int j))
+				ans = Math.Min(ans, i - j);
+			d[Reverse(nums[i])] = i;
+		}
+		if (ans == int.MaxValue)
+			return -1;
+		return ans;
+	}
+	static void Main()
+	{
+		Solution s = new();
+		Console.WriteLine(s.MinMirrorPairDistance([12, 21, 45, 33, 54]));
+		// Console.WriteLine(string.Join(' ', s.MinMirrorPairDistance([12, 21, 45, 33, 54])));
+	}
+}
