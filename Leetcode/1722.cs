cab86c2ae5f0b94f8ae37f71f9ac10c938c951f6
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2026-04-21 10:35:16 +0800

    Add 2 files
    力扣每日
    Leetcode/1722.cs implements MinimumHammingDistance using a UnionFind to group swappable indices and counts mismatches per component.
    Leetcode/2078.cs implements MaxDistance for colors using two-pointer scans from both ends to find maximal index distance.

---
+++ Leetcode/1722.cs
@@ -0,0 +1,48 @@
+﻿class UnionFind(int n)
+{
+	readonly int[] ancestor = [.. Enumerable.Range(0, n)];
+	public int FindAncestor(int n)
+	{
+		if (n != ancestor[n])
+			ancestor[n] = FindAncestor(ancestor[n]);
+		return ancestor[n];
+	}
+	public void Merge(int x, int y) =>
+		ancestor[FindAncestor(x)] = FindAncestor(y);
+}
+public class Solution
+{
+	public int MinimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps)
+	{
+		int ans = 0, n = source.Length;
+		UnionFind uf = new(n);
+		foreach (int[] s in allowedSwaps)
+			uf.Merge(s[0], s[1]);
+		Dictionary<int, List<int>> unionSet = [];
+		for (int i = 0; i < n; i++)
+		{
+			int anc = uf.FindAncestor(i);
+			if (unionSet.TryGetValue(anc, out var v))
+				v.Add(i);
+			else
+				unionSet[anc] = [i];
+		}
+		foreach (List<int> set in unionSet.Values)
+		{
+			Dictionary<int, int> count = [];
+			foreach (int i in set)
+			{
+				count[target[i]] = count.GetValueOrDefault(target[i], 0) + 1;
+				count[source[i]] = count.GetValueOrDefault(source[i], 0) - 1;
+			}
+			ans += count.Values.Where(x => x > 0).Sum();
+		}
+		return ans;
+	}
+	static void Main()
+	{
+		Solution s = new();
+		Console.WriteLine(s.MinimumHammingDistance([1, 2, 3, 4], [2, 1, 4, 5], [[0, 1], [2, 3]]));
+		// Console.WriteLine(string.Join(' ', s.MinimumHammingDistance([1, 2, 3, 4], [2, 1, 4, 5], [[0, 1], [2, 3]])));
+	}
+}
---
+++ Leetcode/2078.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+	public int MaxDistance(int[] colors)
+	{
+		int l = 0, r = colors.Length - 1;
+		for (; colors[l] == colors[r]; l++) ;
+		int ans = r - l;
+		for (l = 0; colors[l] == colors[r]; r--) ;
+		ans = Math.Max(ans, r - l);
+		return ans;
+	}
+	static void Main()
+	{
+		Solution s = new();
+		// Console.WriteLine(s.MinMirrorPairDistance([12, 21, 45, 33, 54]));
+		Console.WriteLine(string.Join(' ', s.MaxDistance([1, 1, 1, 6, 1, 1, 1])));
+	}
+}
