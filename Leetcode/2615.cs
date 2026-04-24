aeedc22d81abe1bb9e1469c7b10e64479ae2585e
JohnLu <49608457+JohnLulzh2002@users.noreply.github.com>
2026-04-24 09:31:09 +0800

    Add 2 files
    力扣每日
    
    2615.cs implements Distance(int[] nums) using two passes with a dictionary to accumulate distances between equal values and returns a long[].
    2833.cs implements FurthestDistanceFromOrigin(string moves) by tracking net position and counting flexible moves ('_') to compute the maximum possible distance.

---
+++ Leetcode/2615.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+	public long[] Distance(int[] nums)
+	{
+		long[] ans = new long[nums.Length], rev = new long[nums.Length];
+		Dictionary<int, (int i, int count)> prev = [];
+		for (int j = 0; j < nums.Length; j++)
+		{
+			if (prev.TryGetValue(nums[j], out var v))
+			{
+				ans[j] = ans[v.i] + (j - v.i) * v.count;
+				prev[nums[j]] = (j, v.count + 1);
+			}
+			else
+				prev[nums[j]] = (j, 1);
+		}
+		prev = [];
+		for (int j = nums.Length - 1; j >= 0; j--)
+		{
+			if (prev.TryGetValue(nums[j], out var v))
+			{
+				rev[j] = rev[v.i] + (v.i - j) * v.count;
+				prev[nums[j]] = (j, v.count + 1);
+			}
+			else
+				prev[nums[j]] = (j, 1);
+		}
+		for (int i = 0; i < nums.Length; i++)
+			ans[i]+=rev[i];
+		return ans;
+	}
+	static void Main()
+	{
+		Solution s = new();
+		// Console.WriteLine(s.f());
+		Console.WriteLine(string.Join(' ', s.Distance([1, 3, 1, 1, 2])));
+	}
+}
---
+++ Leetcode/2833.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+	public int FurthestDistanceFromOrigin(string moves)
+	{
+		int pos = 0, r = 0;
+		foreach (char m in moves)
+		{
+			if (m == 'R')
+				pos++;
+			else if (m == 'L')
+				pos--;
+			else
+				r++;
+		}
+		return Math.Abs(pos) + r;
+	}
+	static void Main()
+	{
+		Solution s = new();
+		Console.WriteLine(s.FurthestDistanceFromOrigin("L_RL__R"));
+		// Console.WriteLine(string.Join(' ', s.f()));
+	}
+}
